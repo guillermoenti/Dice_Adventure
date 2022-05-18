48ea418cbc659be5e69d2f6f4699bd48f68a7421
Guille <guillermo.barrasa@enti.cat>
2022-05-18 19:02:54 +0200

    Idk

---
+++ Assets/Scripts/Enemies/EnemyAI.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private GameObject player;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChasePlayer();
+    }
+
+    private void ChasePlayer()
+    {
+        agent.destination = player.transform.position;
+    }
+}
