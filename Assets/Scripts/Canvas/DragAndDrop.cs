7e2a99f8d0e00f8a0a9013b7021f20cd6ec10e11
Guille <guillermo.barrasa@enti.cat>
2022-06-02 18:05:54 +0200

    Drag

--- Assets/Scripts/Canvas/DragAndDrop.cs
+++ Assets/Scripts/Canvas/DragAndDrop.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class DragAndDrop : MonoBehaviour
+public class DragAndDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    private CanvasGroup canvasGroup;
+    public Transform parentToReturn = null;
+
+    private void Start()
     {
-        
+        canvasGroup = GetComponent<CanvasGroup>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        
+        parentToReturn = transform.parent;
+        canvasGroup.blocksRaycasts = false;
+        transform.SetParent(transform.parent.parent);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canvasGroup.blocksRaycasts = true;
+        transform.SetParent(parentToReturn);
+    }
+
+    public void DestroyItem()
+    {
+        Destroy(gameObject);
     }
 }
